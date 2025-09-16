0aace8f62cc893718f8add5906677077fa4a98ce
KrishnaKumar-project <krishnakumar23070@gmail.com>
2025-09-16 16:50:51 +0530

    user list name changed

--- admin_panel/app/main.py
+++ admin_panel/app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from . import admin_Dashboard, user_block_unblock,database,product_management
+from . import admin_Dashboard, user_block_unblock,database,user_list
 
 app=FastAPI()
 app.include_router(admin_Dashboard.router,tags=["Admin Dashboard."])
 app.include_router(user_block_unblock.router, tags=["Block & Unblock User"])
-app.include_router(product_management.router, tags=["All user list."])
+app.include_router(user_list.router, tags=["All user list."])
 
 @app.on_event("startup")
 def startup():
